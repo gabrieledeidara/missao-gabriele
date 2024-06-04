3dfb095dc004ffec6441358ebe1a5760650679eb
gabrieledeidara <114776941+gabrieledeidara@users.noreply.github.com>
2024-06-04 12:01:27 -0300

    ok

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-const caixaPrincipal = document.querySelector(
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPergunta = document.querySelector(".caixa-pergunta");
+const caixaAlternativa = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    //primeiro objeto dessa lista
+    
+    {enunciado: "pergunta1",
+     alternativas: ["alternativa1",
+        "alternativa2"]
+}
+
+//segundo objeto da lista de perguntas
+
+    enunciado: "pergunta2", 
+    alternativas: ["alternativa1",
+    "alternativa2"]
+]
