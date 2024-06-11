fc76314617740f4b4ad2423ade3b8f51cd5ce122
gabrieledeidara <114776941+gabrieledeidara@users.noreply.github.com>
2024-06-11 11:55:52 -0300

    ok

--- script.js
+++ script.js
@@ -7,14 +7,24 @@ const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
     //primeiro objeto dessa lista
     
-    {enunciado: "pergunta1",
-     alternativas: ["alternativa1",
-        "alternativa2"]
+    {enunciado: "Qual a melhor maneira de combater o desmatamento?",
+     alternativas: ["reflorestamento de áreas degradadas",
+        "implementação de leis mais rigorosas"]
 }
 
 //segundo objeto da lista de perguntas
 
-    enunciado: "pergunta2", 
-    alternativas: ["alternativa1",
-    "alternativa2"]
-]
+   { enunciado: "Como podemos combater a desigualdade social?", 
+    alternativas: ["implementar programas de educação e formação social acessivel a todos",
+    "politicas de redistribuição de renda e oportunidades"]
+},
+     
+];
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){//criando função para mostrar a pergunta
+    perguntaAtual = perguntas[atual]//guardando a lista de perguntas dentro da variavel perguntaAtual
+    caixaPerguntas.textContent = perguntaAtual.enunciado;//manipulando o conteudo do texto e do enunciado 
+}
+mostraPergunta;//executando a função mmostrar pergunta
