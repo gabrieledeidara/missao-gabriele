e36742bde987de714b039dee11574bed1d6f471c
gabrieledeidara <114776941+gabrieledeidara@users.noreply.github.com>
2024-06-11 11:59:52 -0300

    yk

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPergunta = document.querySelector(".caixa-pergunta");
-const caixaAlternativa = document.querySelector(".caixa-alternativa");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativa");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
